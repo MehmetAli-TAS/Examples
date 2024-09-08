d6fc55b7a34df777221a030e424f630d2a7b54e7
Mehmet Ali TAŞ <mmehmetalitas@gmail.com>
2024-09-08 18:10:17 +0300

    Mini machine learning example

---
+++ Mini_machine_learning/main.py
@@ -0,0 +1,46 @@
+#-- Mini machine learning example --#
+
+import matplotlib.pyplot as plt
+import random
+
+def rand():
+    return random.uniform(-10, 10)
+
+def Draw_circle(center, radius, edge_color, inner_filling=None):
+    edge = plt.Circle(center, radius, edgecolor=edge_color, facecolor=inner_filling, fill=inner_filling is not None, linewidth=2)
+    plt.gca().add_patch(edge)
+
+fig, ax = plt.subplots()
+ax.set_aspect('equal')
+
+Draw_circle((0, 0), 10, 'red')
+
+Draw_circle((0, 0), 3, 'black') #The radius of the circle to be found can be assigned any value(3)
+
+#Finding values close to the actual circle radius based on random values and calculating the average.
+h = 100
+h2 = 0
+for i in range(1000):
+    x = rand()
+    y = rand()
+    plt.plot(x, y, marker='o', markersize=5, color='blue') 
+    hypotenuse = (x**2 + y**2) ** 0.5  
+
+    if hypotenuse > 3:
+        if hypotenuse < h:
+            h = hypotenuse
+    else:
+        if hypotenuse > h2:
+            h2 = hypotenuse
+
+print(f"The nearest distance from outside the circle: {h:.5f}")
+print(f"The nearest distance from inside the circle: {h2:.5f}")
+print(f"Output value: {(h+h2)/2:.5f}")
+
+
+plt.xlim(-11, 11)
+plt.ylim(-11, 11)
+plt.gca().set_facecolor('white')
+plt.axis('on')
+
+plt.show()
--- tests/main.py
+++ tests/main.py
@@ -3,35 +3,48 @@ import random
 
 def rand():
     return random.randint(-10, 10)
-# Daire çizmek için bir fonksiyon
+
+
 def ciz_daire(merkez, yaricap, kenar_rengi, ic_dolgu=None):
     daire = plt.Circle(merkez, yaricap, edgecolor=kenar_rengi, facecolor=ic_dolgu, fill=ic_dolgu is not None, linewidth=2)
     plt.gca().add_patch(daire)
 
-# Grafik ayarları
+
+
 fig, ax = plt.subplots()
 ax.set_aspect('equal')
 
-# Dış kırmızı boş çember
-ciz_daire((0, 0), 10, 'red')  # Dış çemberin yarıçapı 10 (çapı 20)
-ciz_daire((0, 0), 3, 'black')  # Dış çemberin yarıçapı 10 (çapı 20)
+ciz_daire((0, 0), 10, 'red') 
+ciz_daire((0, 0), 3, 'black') 
 
-
-for i in range(10):
+tahmini_cap = rand()
+h = 100
+h2 = 0
+for i in range(10000):
     x = rand()
     y = rand()
     plt.plot(x, y, marker='o', markersize=5, color='blue')
     hipotenus = (x**2 + y**2) ** 1/2
     
-    if(hipotenus > 3):
-        print("nokta çember dışında")
+    if hipotenus > 3:
+        if hipotenus < h:
+            h = hipotenus
     else:
-        print("nokta çember içinde")
+        if hipotenus > h2:
+            h2 = hipotenus
+    
+    
+    
+print(h, h2)      
+print((h+h2)/2)
+
+
+
 
 plt.xlim(-11, 11)
 plt.ylim(-11, 11)
-plt.gca().set_facecolor('white')   # Arka plan rengi beyaz
-plt.axis('on')                    # Eksenleri gizle
+plt.gca().set_facecolor('white')  
+plt.axis('on')                   
 
 
 plt.show()
